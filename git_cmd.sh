3ebebafcb9f7de580804282d71d76e6c47623461
kazushi <kimoto@okayama-u.ac.jp>
2020-03-30 16:21:25 +0900

    2nd commit

--- git_cmd.sh
+++ git_cmd.sh
@@ -1,10 +1,8 @@
 git add git_cmd.sh
 
-git add 2020/Moodle
-git add *.docx
-git add *.pdf
-
-git add git_cmd.sh
+#git add 2020/Moodle/
+#git add *.docx
+#git add *.pdf
 
 git commit
 git push -u origin master
