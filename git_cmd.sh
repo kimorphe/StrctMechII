a4fc966f6de5341aa632a98bc2d4eaaf5e308b88
kazushi <kimoto@okayama-u.ac.jp>
2020-03-30 16:23:14 +0900

    Lec1 folder opened

--- git_cmd.sh
+++ git_cmd.sh
@@ -1,7 +1,8 @@
 git add git_cmd.sh
-#git add 2020/Moodle/
-#git add *.docx
-#git add *.pdf
+git add 2020/Moodle/
+git add 2020/Lec1
+git add *.docx
+git add *.pdf
 
 git commit
 git push -u origin master
