5cfe120bf2ec139ae9c1103ba5e159aec7e50db9
kazushi <kimoto@okayama-u.ac.jp>
2020-04-01 17:37:03 +0900

    Lec 1 almost there

--- git_cmd.sh
+++ git_cmd.sh
@@ -1,10 +1,10 @@
 git add git_cmd.sh
 git add 2020/Moodle/
-git add 2020/Lec1/*.tex
-git add 2020/Lec1/*.txt
-git add 2020/Lec1/*.svgz
-git add *.docx
-git add *.pdf
+git add 2020/Lec*/*.tex
+git add 2020/Lec*/*.txt
+git add 2020/Lec*/*.svgz
+git add 2020/Lec*/makefile
+git add ./*.docx
 
 git commit
 git push -u origin master
